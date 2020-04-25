16aade3f16a822a081442eadd75b4e2ed9573b28
vlajs16 <vladimir.belca@gmail.com>
2020-04-25 16:41:39 +0200

    Added UseCors to API

--- API/Startup.cs
+++ API/Startup.cs
@@ -69,6 +69,7 @@ namespace API
 
             //app.UseHttpsRedirection();
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting();
 
             app.UseAuthentication();
